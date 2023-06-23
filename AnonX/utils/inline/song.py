7a8f1a1dbc0c15ec945f459d98a39a8c623f83a1
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2023-06-23 14:56:40 +0530

    Update song.py

--- AnonX/utils/inline/song.py
+++ AnonX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_GROUP}",
+                text="🌷𝐒υρρσɾ𝐓🌷", url=f"{config.SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
