b5d322aaf7b4dbfec1a365e65be0c3b1f043f482
Rishii_x_Janni <118364756+Rishii7205@users.noreply.github.com>
2023-07-06 14:52:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5908843784").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/rishii7205/jam")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/rishii7205/button")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
