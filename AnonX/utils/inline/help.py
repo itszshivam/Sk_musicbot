f2cff31bee827f96fdbf08ba62c4f6bf8b0b3a46
Rishii_x_Janni <118364756+Rishii7205@users.noreply.github.com>
2023-07-06 18:05:47 +0530

    Update help.py

--- AnonX/utils/inline/help.py
+++ AnonX/utils/inline/help.py
@@ -23,65 +23,8 @@ def help_pannel(_, START: Union[bool, int] = None):
         [
             [
                 InlineKeyboardButton(
-                    text="💘𝐀∂мι𝐍💘",
-                    callback_data="help_callback hb1",
-                ),
-                InlineKeyboardButton(
-                    text="🌷𝐀υт𝐇🌷",
-                    callback_data="help_callback hb2",
-                ),
-                InlineKeyboardButton(
-                    text="🌹𝐁ℓα¢ℓιѕ𝐓🌹",
-                    callback_data="help_callback hb3",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="🌸𝐆¢αѕ𝐓🌸",
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text="🍒𝐆вα𝐍🍒",
-                    callback_data="help_callback hb12",
-                ),
-                InlineKeyboardButton(
-                    text="🥰𝐋уяι¢𝐒🥰",
-                    callback_data="help_callback hb5",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="✨𝐏ιи𝐆✨",
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text="⚡𝐏ℓα𝐘⚡",
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text="🎄𝐏ℓαуℓιѕ𝐓🎄",
-                    callback_data="help_callback hb6",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="💓𝐕ι∂єσ¢нαт𝐒💓",
-                    callback_data="help_callback hb10",
-                ),
-                InlineKeyboardButton(
-                    text="❤𝐒тαя𝐓❤",
-                    callback_data="help_callback hb11",
-                ),
-                InlineKeyboardButton(
-                    text="❣️𝐒υ∂𝐎❣️",
-                    callback_data="help_callback hb9",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="💓𝐄χтя𝐀💓",
-                    callback_data="help_callback hb13",
-                
+                    text="💘𝐎𝐖𝐍𝐄𝐑💘",
+                    url="https//t.me/your_godfather_xd",
                 ),
             ],
             mark,
